8ab307ad1b8ccd6c497f6c09f1413558985ac131
aco <wwsxuan@163.com>
2018-05-23 17:12:10 +0800

    运动形式

--- test/test.js
+++ test/test.js
@@ -6,17 +6,32 @@ var doms = mMove.initDom(app, 'm-move-wrap', 7, 9)
 
 var mc = new mMove.MC(7, 9)
 
+var animateType = 'test'
+
 mc.$on('movePoint', ({point, mode}) => {
-  doms[point.x - 1][point.y - 1].children[0].style = `background-image:url(${url});`
-  doms[point.x - 1][point.y - 1].style = `background-image:url(${url});`
+  let dom = doms[point.x - 1][point.y - 1]
+  if (dom.dataset.change) {
+    return
+  }
+  let oldClass = dom.className
+  dom.className = `${oldClass} ${animateType}`
+  dom.dataset.change = '1'
+  dom.style.transition = mode.duration / 1000 + 's'
+  dom.addEventListener('transitionend', function listen() {
+    console.log(oldClass)
+    dom.children[0].style.backgroundImage = `url(${url})`
+    dom.className = oldClass
+    dom.removeEventListener('transitionend', listen)
+    dom.dataset.change = ''
+  })
 })
 
-mc.movePoint(mMove.mode[0])
+mc.movePoint(mMove.mode[8])
 
 function getRandom(min, max) {
-  return Math.ceil(Math.random() * (max - min)) + min
+  return Math.round(Math.random() * (max - min)) + min
 }
 
 app.addEventListener('click', () => {
-  mc.movePoint(mMove.mode[0])
+  mc.movePoint(mMove.mode[getRandom(0, mMove.mode.length - 1)])
 })

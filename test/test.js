38937035ec71bce6def189e0a60d30279f626116
aco <wwsxuan@163.com>
2018-05-24 20:38:36 +0800

    兼容animation动画

--- test/test.js
+++ test/test.js
@@ -5,7 +5,9 @@ var urls = ['http://7xse2z.com1.z0.glb.clouddn.com/257084.jpg',
   'http://7xse2z.com1.z0.glb.clouddn.com/3455%20%281%29.jpg']
 
 var move = mMove.makeMatrixChange(app, {
-  images: urls
+  images: urls,
+  row: 7,
+  col: 9
 })
 
 function getRandom(min, max) {
@@ -17,7 +19,7 @@ move.movePoint(mMove.mode[5])
 app.addEventListener('click', function () {
   move.movePoint(mMove.mode[getRandom(0, mMove.mode.length - 1)], {
     classNameIn: 'animated flipInX',
-    classNameOut: 'animated flipOutY',
+    classNameOut: 'animated flipOutX',
     animate: true
   })
 })

43f1a037f40edff7d1c2fd11be18c708b3425ed9
aco <wwsxuan@163.com>
2018-05-24 18:16:46 +0800

    兼容animation动画

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export {Matrix} from './lib/matrix'
 export {initStyle, initDom} from './lib/initHtml'
 export {mode} from './lib/motionMode'
+export {makeMatrixChange} from './lib/makeMatrixChange'
--- test/test.js
+++ test/test.js
@@ -4,48 +4,20 @@ var urls = ['http://7xse2z.com1.z0.glb.clouddn.com/257084.jpg',
   'http://7xse2z.com1.z0.glb.clouddn.com/285848.jpg',
   'http://7xse2z.com1.z0.glb.clouddn.com/3455%20%281%29.jpg']
 
-var row = 7
-var col = 9
-
-mMove.initStyle('m-move-wrap', row, col)
-var doms = mMove.initDom(app, 'm-move-wrap', row, col)
-
-var mm = new mMove.Matrix(row, col)
-
-var animateClass = 'test'
-
-var url = urls[0]
+var move = mMove.makeMatrixChange(app, {
+  images: urls
+})
 
 function getRandom(min, max) {
   return Math.round(Math.random() * (max - min)) + min
 }
 
-mm.$on('changeStart', function () {
-  url = urls[getRandom(0, urls.length - 1)]
-})
-
-mm.$on('hitPoint', function (e) {
-  var point = e.point
-  var mode = e.mode
-  var dom = doms[point.x][point.y]
-  if (dom.dataset.change) {
-    return
-  }
-  var oldClass = dom.className
-  dom.className = `${oldClass} ${animateClass}`
-  dom.dataset.change = '1'
-  dom.style.transition = mode.duration / 1000 + 's'
-  dom.addEventListener('transitionend', function listen() {
-    console.log(url)
-    dom.children[0].style.backgroundImage = `url(${url})`
-    dom.className = oldClass
-    dom.removeEventListener('transitionend', listen)
-    dom.dataset.change = ''
-  })
-})
-
-mm.movePoint(mMove.mode[0])
+move.movePoint(mMove.mode[5])
 
 app.addEventListener('click', function () {
-  mm.movePoint(mMove.mode[getRandom(0, mMove.mode.length - 1)])
+  move.movePoint(mMove.mode[getRandom(0, mMove.mode.length - 1)], {
+    classNameIn: 'animated flipInX',
+    classNameOut: 'animated flipOutY',
+    animate: true
+  })
 })

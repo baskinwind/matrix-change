f243c3b0a8749706f2f4092b11dcce971b620da2
aco <wwsxuan@163.com>
2018-05-28 15:14:19 +0800

    demo 修改

---
+++ demo/index.js
@@ -0,0 +1,40 @@
+var app = document.getElementById('app')
+var urls = ['http://7xse2z.com1.z0.glb.clouddn.com/257084.jpg',
+  'http://7xse2z.com1.z0.glb.clouddn.com/257453.jpg',
+  'http://7xse2z.com1.z0.glb.clouddn.com/285848.jpg',
+  'http://7xse2z.com1.z0.glb.clouddn.com/3455%20%281%29.jpg',
+  'http://7xse2z.com1.z0.glb.clouddn.com/280752.jpg',
+  'http://7xse2z.com1.z0.glb.clouddn.com/286477.jpg',
+  'http://7xse2z.com1.z0.glb.clouddn.com/257508.jpg',
+  'http://7xse2z.com1.z0.glb.clouddn.com/wallhaven-406224.png']
+
+var move = mChange.makeMatrixChange(app, {
+  images: urls,
+  row: 7,
+  col: 9
+})
+
+function getRandom(min, max) {
+  return Math.round(Math.random() * (max - min)) + min
+}
+
+move.movePoint(mChange.mode[0])
+
+new Vue({
+  el: '#option',
+  data: {
+    inSelect: 'flash',
+    inList: ['flash', 'rubberBand', 'shake', 'headShake', 'swing', 'tada', 'wobble', 'jello', 'bounceOut', 'bounceOutDown', 'bounceOutLeft', 'bounceOutRight', 'bounceOutUp', 'fadeOut', 'fadeOutDown', 'fadeOutDownBig', 'fadeOutLeft', 'fadeOutLeftBig', 'fadeOutRight', 'fadeOutRightBig', 'fadeOutUp', 'fadeOutUpBig', 'flipOutX', 'flipOutY', 'lightSpeedOut', 'rotateOut', 'rotateOutDownLeft', 'rotateOutDownRight', 'rotateOutUpLeft', 'rotateOutUpRight', 'hinge', 'rollOut', 'zoomOut', 'zoomOutDown', 'zoomOutLeft', 'zoomOutRight', 'zoomOutUp', 'slideOutDown', 'slideOutLeft', 'slideOutRight', 'slideOutUp'],
+    outSelect: 'flash',
+    outList: ['flash', 'rubberBand', 'shake', 'headShake', 'swing', 'tada', 'wobble', 'jello', 'bounceIn', 'bounceInDown', 'bounceInLeft', 'bounceInRight', 'bounceInUp', 'fadeIn', 'fadeInDown', 'fadeInDownBig', 'fadeInLeft', 'fadeInLeftBig', 'fadeInRight', 'fadeInRightBig', 'fadeInUp', 'fadeInUpBig', 'flipInX', 'flipInY', 'lightSpeedIn', 'rotateIn', 'rotateInDownLeft', 'rotateInDownRight', 'rotateInUpLeft', 'rotateInUpRight', 'rollIn', 'zoomIn', 'zoomInDown', 'zoomInLeft', 'zoomInRight', 'zoomInUp', 'slideInDown', 'slideInLeft', 'slideInRight', 'slideInUp']
+  },
+  methods: {
+    start() {
+      move.movePoint(mChange.mode[getRandom(0, mChange.mode.length - 1)], {
+        animate: true,
+        classNameIn: 'animated ' + this.inSelect,
+        classNameOut: 'animated ' + this.outSelect
+      })
+    }
+  }
+})

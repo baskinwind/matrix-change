5354bffee1b021263936085ab1900c7e68cc4a0a
aco <wwsxuan@163.com>
2018-07-09 15:27:17 +0800

    version & ES6

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-let Matrix = require('./lib/Matrix')
-let {containerLayout, initDom} = require('./lib/initHtml')
-let mode = require('./lib/motionMode')
-let makeMatrixChange = require('./lib/makeMatrixChange')
-
-module.exports = {
-  Matrix: Matrix,
-  containerLayout: containerLayout,
-  initDom: initDom,
-  mode: mode,
-  makeMatrixChange: makeMatrixChange,
-}
+export * from './lib/Matrix'
+export * from './lib/initHtml'
+export * from './lib/motionMode'
+export * from './lib/makeMatrixChange'
